8ac1deb8f43b0a98aa6417931f77f687d40ceda6
mendurim <mendurim.r@gmail.com>
2016-04-14 20:35:11 +0200

    Added the pigLatin Algorithm

---
+++ pigLatin/main.js
@@ -0,0 +1,11 @@
+function translatePigLatin(str) {
+  var vowelCheck = /[aeiouy]/i; // regex vowel check
+  if(vowelCheck.test(str[0])){
+   return str + "ay"; 
+  }else{
+    var con = str.search(vowelCheck); 
+    return str.slice(con) + str.slice(0, con) + "ay"; 
+  }
+}
+translatePigLatin("consonant");
+
