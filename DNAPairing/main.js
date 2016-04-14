12fb23be3060d609592d15fd29dd97206eb64ef5
mendurim <mendurim.r@gmail.com>
2016-04-14 21:04:46 +0200

    Added the DNAPairing solution

---
+++ DNAPairing/main.js
@@ -0,0 +1,18 @@
+function pairElement(str){
+    
+    // the object containing pairing keys and values
+    var pairing = {
+    	"A":"T", 
+    	"T": "A", 
+    	"C":"G",
+    	"G":"C"
+    }, 
+    pairedArray = []; 
+    //split the pairedArray and map it by using the object pairing
+    pairedArray = str.split("").map(function(key){
+    	var newArr = []; 
+    	newArr.push(key, pairing[key]); 
+    	return newArr; 
+    }); 
+	return pairedArray; 
+}
