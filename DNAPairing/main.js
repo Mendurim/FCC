b5f6e794b7c5b8e3488edb8d8a869975da9e1e7b
mendurim <mendurim.r@gmail.com>
2016-04-18 14:07:19 +0200

    Minor fix

--- DNAPairing/main.js
+++ DNAPairing/main.js
@@ -1,10 +1,11 @@
 
 function pairElement(str){
     
-    // the object containing pairing keys and value
+    // the object containing pairing keys and value; 
     
     var pairing = {"A":"T", "T": "A", "C":"G","G":"C"}, pairedArray = []; 
     //split the pairedArray and map it by using the object pairing
+    
     
     pairedArray = str.split("").map(function(key){
     	
