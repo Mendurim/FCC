8de249761f0842e4990a739586b54d269946f213
mendurim <mendurim.r@gmail.com>
2016-04-20 21:52:30 +0200

    Minor refac

--- pigLatin/main.js
+++ pigLatin/main.js
@@ -1,8 +1,12 @@
 function translatePigLatin(str) {
   var vowelCheck = /[aeiouy]/i; // regex vowel check
+  
   if(vowelCheck.test(str[0])){
+  
    return str + "ay"; 
-  }else{
+  
+  }else
+  {
     var con = str.search(vowelCheck); 
     return str.slice(con) + str.slice(0, con) + "ay"; 
   }
