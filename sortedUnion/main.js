d5260c45e09c577a76ffa1955c3631fa2723e058
mendurim <mendurim.r@gmail.com>
2016-04-25 23:32:26 +0200

    Refac

--- sortedUnion/main.js
+++ sortedUnion/main.js
@@ -1,11 +1,11 @@
 function uniteUnique(arr1, arr2, arr3){
+
 // flatening
-var arrNew = arr1.concat(arr2, arr3); 
-return arrNew.reduce(function(i){
-return (arr1.indexOf(i)===-1 || arr2.indexOf(i)===-1); 
-
-})
-
+	var arrNew = arr1.concat(arr2, arr3); 
+	
+	return arrNew.reduce(function(i){
+	return (arr1.indexOf(i)===-1 || arr2.indexOf(i)===-1); 
+	}); 
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
