d648a07ceaa0e4fbda914d7ddd46f1fbe6194780
mendurim <mendurim.r@gmail.com>
2016-04-27 07:44:01 +0200

    final push

--- sortedUnion/main.js
+++ sortedUnion/main.js
@@ -1,12 +1,11 @@
 function uniteUnique(arr1, arr2, arr3){
 
-// flatening
-	var arrNew = arr1.concat(arr2, arr3); 
-	
-	return arrNew.reduce(function(i){
-	
-	return (arr1.indexOf(i)===-1 || arr2.indexOf(i)===-1); 
-	}); 
+ var args = Array.prototype.slice.call(arguments); 
+ var mergedArray = [].concat.apply([], args); 
+ var uniqueArray = mergedArray.filter(function(el,pos,arr){
+ 	return arr.indexOf(el)==pos; 
+ });
+ return uniqueArray; 
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
